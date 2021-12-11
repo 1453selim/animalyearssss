af64fe974c7c3957e457ef90d3cb4c9d38e8be9f
selim isabetli <37.selim1543@gmail.com>
2021-12-11 16:29:23 +0300

    javascript olusturuldu

---
+++ script.js
@@ -0,0 +1,20 @@
+var myAge = prompt("write your age");
+var myName = prompt("write your name").toLowerCase()
+//var myAge = 35;
+//benim yasım
+var earlyYears = 2;
+//kopegin ilk iki yılı
+var result = earlyYears*10.5;
+console.log(result);
+var laterYears = myAge-2;
+console.log(laterYears);
+var myLateryears = laterYears*4;
+console.log(myLateryears);//my later tears for for years
+const myAgeInDogYears = myLateryears + result;
+console.log(myAgeInDogYears);// myage according to dogage years
+//var myName = "Selim İsabetli"
+console.log("My name is " + myName +" I am " +myAge + "years old in human years which is  " +myAgeInDogYears +" years old in dog years" )
+
+
+
+
